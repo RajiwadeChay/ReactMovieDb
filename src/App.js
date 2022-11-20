fe7f158565e4f9f04939e984b4f85f15598bd72c
RajiwadeChay <rajiwadechaitanya@gmail.com>
2022-11-20 20:39:48 +0530

    Page responsive html css done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
 import './App.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import MainBody from './components/MainBody';
+// import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='container'>
+      {/* <Header />
+      <MainBody />
+      <Footer /> */}
+      <header>
+        <div className="wrapper">
+          <h1>
+            <a href="/" title="Home Page">MovieDb</a>
+          </h1>
+          <div className="hamburger">
+            <span className="bar">bar</span>
+          </div>
+          <nav>
+            <ul>
+              <li><a href="/components/Popular">Popular</a></li>
+              <li><a href="/components/TopRated">Top Rated</a></li>
+              <li><a href="/components/Upcoming">Upcoming</a></li>
+            </ul>
+          </nav>
+          <form className="search-form">
+            <div className='search-bar'>
+              <label htmlFor="searchBar"></label>
+              <input type="text" id="searchBar" placeholder="Movie Name" />
+            </div>
+            <div className="search-submit">
+              <input type="submit" value="&#128269;" />
+            </div>
+          </form>
+        </div>
       </header>
+      <main>
+        <div className="wrapper">
+          <ul className="movie-list">
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+            <li>
+              <figure>
+                <img src="https://via.placeholder.com/245X330" alt="Movie Poster" />
+              </figure>
+              <p className='movie-name'><a href="#FIXME" title='Movie Name'>Movie Name</a></p>
+              <p className='rating'>Rating</p>
+            </li>
+
+          </ul>
+        </div>
+      </main>
+      <footer>
+        <div className="wrapper">
+          <p>&copy; MovieDb Copy Rights Reserved 2022</p>
+          <div className="footer-logo">
+            <a href="/" title="Home Page">MovieDb</a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
+import { Nav, NavLink, NavMenu } from "./NavbarElements";
+
 export default function Header() {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <Nav>
+                <NavMenu>
+                    <NavLink to="/Popular" activeStyle>
+                        Popular
+                    </NavLink>
+                    <NavLink to="/TopRated" activeStyle>
+                        Top Rated
+                    </NavLink>
+                    <NavLink to="/Upcoming" activeStyle>
+                        Upcoming
+                    </NavLink>
+                </NavMenu>
+            </Nav>
+        </>
     );
 }
